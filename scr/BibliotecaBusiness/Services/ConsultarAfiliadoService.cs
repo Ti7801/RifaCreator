e68ea20baff1370244b1ba64c3ebede525b83631
Ti7801 <tiagodaltro19@gmail.com>
2024-09-16 10:42:34 -0300

    Construindo o serviço ConsultarAfiladoService.

---
+++ scr/BibliotecaBusiness/Services/ConsultarAfiliadoService.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class ConsultarAfiliadoService
+    {
+        private readonly IAfiliadoRepository afiliadoRepository;
+
+        public ConsultarAfiliadoService(IAfiliadoRepository afiliadoRepository)
+        {
+            this.afiliadoRepository = afiliadoRepository;   
+        }
+
+        public ServiceResult ConsultarAfiliado(Afiliado afiliado)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                serviceResult.Success = true;
+                afiliadoRepository.AtualizarAfiliado(afiliado);
+            }
+            catch (Exception e) 
+            {
+                serviceResult.Success= false;   
+                serviceResult.Erros.Add(e.Message); 
+            }
+
+            return serviceResult;
+        }
+    }
+}
