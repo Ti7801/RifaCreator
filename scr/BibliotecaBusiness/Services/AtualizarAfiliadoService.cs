70e2bc4ec3f24c6460cbf169b3cddeb1faba36b7
Ti7801 <tiagodaltro19@gmail.com>
2024-09-18 11:33:24 -0300

    Adicionando novos serviços: AtualizarAfiliadoService e ExcluirAfiliadoService.

---
+++ scr/BibliotecaBusiness/Services/AtualizarAfiliadoService.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class AtualizarAfiliadoService
+    {
+        private readonly IAfiliadoRepository afiliadoRepository;
+
+        public AtualizarAfiliadoService(IAfiliadoRepository afiliadoRepository)
+        {
+            this.afiliadoRepository = afiliadoRepository;
+        }
+
+        public ServiceResult AtualizarAfiliado(Afiliado afiliado)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try 
+            {
+                afiliadoRepository.AtualizarAfiliado(afiliado);
+                serviceResult.Success = true;   
+            }
+            catch (Exception e) 
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+
+            return serviceResult;
+        }
+    }
+}
---
+++ scr/BibliotecaBusiness/Services/ExcluirAfiliadoService.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
+{
+    public class ExcluirAfiliadoService
+    {
+        private readonly IAfiliadoRepository afiliadoRepository;
+
+        public ExcluirAfiliadoService(IAfiliadoRepository afiliadoRepository)
+        {
+            this.afiliadoRepository = afiliadoRepository;
+        }
+
+        public ServiceResult ExcluirAfiliado(Afiliado afiliado)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try 
+            {
+                afiliadoRepository.RemoverAfiliado(afiliado);
+            }
+            catch (Exception e) 
+            { 
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message);
+            }
+
+            return serviceResult;
+        }
+    }
+}
