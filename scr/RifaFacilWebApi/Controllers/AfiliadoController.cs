5ec7239ecd160f5c3251dfbfa1b89557b792859f
Ti7801 <tiagodaltro19@gmail.com>
2024-09-18 12:17:42 -0300

    Criando a ActionResult CadastrarAfiliado do verbo HttpPost.

--- scr/RifaFacilWebApi/Controllers/AfiliadoController.cs
+++ scr/RifaFacilWebApi/Controllers/AfiliadoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BibliotecaBusiness.Models;
+using BibliotecaBusiness.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RifaFacilWebApi.Controllers
 {
@@ -6,6 +8,46 @@ namespace RifaFacilWebApi.Controllers
     [Route("afiliado")]
     public class AfiliadoController : ControllerBase
     {
-        
+        private readonly CadastrarAfiliadoService cadastrarAfiliadoService;
+        //private readonly ConsultarAfiliadoService consultarAfiliadoService;
+        //private readonly AtualizarAfiliadoService atualizarAfiliadoService; 
+        //private readonly ExcluirAfiliadoService excluirAfiliadoService; 
+
+        public AfiliadoController(CadastrarAfiliadoService cadastrarAfiliadoService//,
+                                 //ConsultarAfiliadoService consultarAfiliadoService,
+                                 //AtualizarAfiliadoService atualizarAfiliadoService,
+                                 //ExcluirAfiliadoService excluirAfiliadoService
+                                 )
+        {
+            this.cadastrarAfiliadoService = cadastrarAfiliadoService;
+            //this.consultarAfiliadoService = consultarAfiliadoService;
+            //this.atualizarAfiliadoService = atualizarAfiliadoService;
+            //this.excluirAfiliadoService = excluirAfiliadoService;
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Afiliado), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Afiliado> CadastrarAfiliado(Afiliado afiliado)
+        {
+            if (!ModelState.IsValid)
+            {
+                var erros = ModelState.Values.SelectMany(X => X.Errors).Select(erros => erros.ErrorMessage).SingleOrDefault();
+
+                return BadRequest(erros);   
+            }
+
+            ServiceResult serviceResult = cadastrarAfiliadoService.CadastrarAfiliado(afiliado);
+
+            if (!serviceResult.Success)
+            {
+                return BadRequest(serviceResult.Erros);
+            }
+
+
+            return CreatedAtAction(nameof(CadastrarAfiliado), afiliado);
+        }
+      
     }
 }
