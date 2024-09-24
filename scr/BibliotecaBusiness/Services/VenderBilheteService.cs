dad10995473092c3be7efc8e9cbad9a6508a46b9
Ti7801 <tiagodaltro19@gmail.com>
2024-09-24 11:21:46 -0300

    Criando o serviço VenderBilheteService

--- scr/BibliotecaBusiness/Services/VenderBilheteService.cs
+++ scr/BibliotecaBusiness/Services/VenderBilheteService.cs
@@ -1,6 +1,33 @@
-﻿namespace BibliotecaBusiness.Services
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+
+namespace BibliotecaBusiness.Services
 {
-    public class VenderBilheteService
+    public class VenderBilheteService 
     {
+        private readonly IBilheteRepository bilheteRepository;
+
+        public VenderBilheteService(IBilheteRepository bilheteRepository)
+        {
+            this.bilheteRepository = bilheteRepository;
+        }
+
+        public ServiceResult VenderBilhete(Bilhete bilhete)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+
+            try
+            {
+                bilheteRepository.AdicionarBilhete(bilhete);
+                serviceResult.Success = true;
+            }
+            catch (Exception e) 
+            {
+                serviceResult.Success = false;
+                serviceResult.Erros.Add(e.Message); 
+            }
+
+            return serviceResult;
+        }
     }
 }
