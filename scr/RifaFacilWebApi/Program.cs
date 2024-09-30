d75dc6542a260604a9fff98c534bf483bd7a1b49
Ti7801 <tiagodaltro19@gmail.com>
2024-09-30 12:39:54 -0300

    Fazendo configurações para estabelecer a Autenticação. Em moldels, program e appsettings.json

--- scr/RifaFacilWebApi/Program.cs
+++ scr/RifaFacilWebApi/Program.cs
@@ -1,8 +1,12 @@
 using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
 using BibliotecaBusiness.Services;
 using BibliotecaData.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +20,6 @@ builder.Services.AddDbContext<AppDbContext>(
     },
     ServiceLifetime.Scoped
 );
-
-//Autentication - Uso da Identidade - USUARIO/PERFIL
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>();
 
 
 builder.Services.AddScoped<IRifaRepository, RifaRepository>();
@@ -48,6 +47,37 @@ builder.Services.AddScoped<ConsultarAfiliadoService>();
 builder.Services.AddScoped<ConsultarAfiliadoPorIdService>();
 builder.Services.AddScoped<AtualizarAfiliadoService>();
 builder.Services.AddScoped<ExcluirAfiliadoService>();
+
+
+//Autentication - Uso da Identidade - USUARIO/PERFIL
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+//Pegando o Token e gerando a chave encodada
+var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
+var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audiencia,
+        ValidIssuer = jwtSettings.Emissor
+    };
+});
 
 
 builder.Services.AddControllers();
